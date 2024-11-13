392c66e92e0d17df39829bfae129725b0624f1d2
Pedro Gabriel Sousa Lopes <pedrgabriel_@outlook.com>
2024-11-13 16:12:13 -0300

    Data validation and consistency checks
    Validations and consistency checks were performed on the data provided by the user.

--- src/Main.java
+++ src/Main.java
@@ -11,21 +11,65 @@ public class Main {
 		int destino;
 
 		System.out.println("----- GRAFOS -----");
-		System.out.println("Informe a quantidade de vértices: ");
-		numVertices = s.nextInt();
-		System.out.println("Informe a quantidade de arestas: ");
-		numArestas = s.nextInt();
+		
+		while (true) {
+			System.out.println("Informe a quantidade de vértices: ");
+			try {
+				numVertices = Integer.parseInt(s.next());
+				if (numVertices > 0) {
+					break;
+				} else {
+					System.out.println("Erro! Por favor, informe um número inteiro positivo.");
+				}
+			} catch (NumberFormatException ex) {
+				System.out.println("Erro! Por favor, informe um número inteiro.");
+			}
+		}
+		
+		while (true) {
+			System.out.println("Informe a quantidade de arestas: ");
+			try {
+				numArestas = Integer.parseInt(s.next());
+				if (numArestas > 0) {
+					break;
+				} else {
+					System.out.println("Erro! Por favor, informe um número inteiro positivo.");
+				}
+			} catch (NumberFormatException ex) {
+				System.out.println("Erro! Por favor, informe um número inteiro.");
+			}
+		}
 
 		Grafo grafo = new Grafo(numVertices, numArestas);
 
 		System.out.println("Informe as arestas no formato {origem,destino} (sem espaços e com vírgula): ");
 		for (int i = 0; i < numArestas; i++) {
-			aresta = s.next();
-			aresta = aresta.replace("{", "").replace("}", "");
-			vertices = aresta.split(",");
-			origem = Integer.parseInt(vertices[0]);
-			destino = Integer.parseInt(vertices[1]);
-			grafo.adicionarAresta(origem, destino);
+			while(true) {
+				aresta = s.next();
+				aresta = aresta.replace("{", "").replace("}", "");
+				vertices = aresta.split(",");
+				if (vertices.length != 2) {
+					System.out.println("Erro! O formato informado é inválido.");
+					continue;
+				}
+				try {
+					origem = Integer.parseInt(vertices[0]);
+					destino = Integer.parseInt(vertices[1]);
+					/*
+					 * Validação considerando que qualquer aresta informada tenha como menor valor o 1
+					 * ou
+					 * tenha o valor menor/igual ao número de vértices
+					 */
+					if (origem < 1 || origem > numVertices || destino < 1 || destino > numVertices) {
+						System.out.println("Erro! Por favor, informe vértices que estejam dentro do escopo 1 a " + numVertices + ".");
+					} else {
+						grafo.adicionarAresta(origem, destino);
+						break;
+					}
+				} catch (NumberFormatException ex) {
+					System.out.println("Erro! Por favor, informe um número inteiro.");
+				}
+			}
 		}
 		grafo.preencheMatriz();
 		grafo.imprimeMatriz();
